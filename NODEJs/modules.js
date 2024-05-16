2238b30a27e436d909031a70758cd9d7656f8433
sravanthi <sravanthireddygangula0804@gmail.com>
2024-05-16 13:54:54 +0530

    Update files

--- NODEJs/modules.js
+++ NODEJs/modules.js
@@ -2,8 +2,9 @@ const events= require('events')
 const http= require('http')
 http.createServer((req,res)=>
 {
-    
-})
+    res.write("Hello everyone")
+    res.end();
+}).listen(3000);
 var myEmmitter = new events.EventEmitter();
 myEmmitter.on("someevent",()=>
 {
