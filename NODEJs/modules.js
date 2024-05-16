af3969594178ebbea42098c78d9ca9f2418b0269
sravanthi <sravanthireddygangula0804@gmail.com>
2024-05-16 14:32:53 +0530

    third commit with css

--- NODEJs/modules.js
+++ NODEJs/modules.js
@@ -1,13 +1,13 @@
 const events= require('events')
 const http= require('http')
-http.createServer((req,res)=>
-{
-    res.write("Hello everyone")
-    res.end();
-}).listen(3000);
+
 var myEmmitter = new events.EventEmitter();
 myEmmitter.on("someevent",()=>
 {
-    console.log(`some event is fired`);
+    http.createServer((req,res)=>
+        {
+            res.write("Hello everyone from some event")
+            res.end();
+        }).listen(3000);
 })
 myEmmitter.emit("someevent","welcome")
